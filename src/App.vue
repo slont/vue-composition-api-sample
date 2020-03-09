b7c3983626edb2bd8047903904df4cfb133b3d30
slont <slont.maytry@gmail.com>
2020-03-09 16:11:07 +0900

    画面に反映

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/composition-api">Composition API</router-link> |
+      <router-link to="/vue-syntax">Vue Syntax</router-link>
     </div>
-    <router-view/>
+    <transition name="slide-fade" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<style lang="sass">
+  #app
+    font-family: Avenir, Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #2c3e50
+    margin: auto
+    width: 320px
+  #nav
+    padding: 30px
+    a
+      font-weight: bold
+      color: #2c3e50
+      &.router-link-exact-active
+        color: #42b983
 
-#nav {
-  padding: 30px;
+  .slide-fade-enter-active,
+  .slide-fade-leave-active
+    transition: all .2s ease
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+  .slide-fade-enter,
+  .slide-fade-leave-to
+    transform: translateX(-10px)
+    opacity: 0
 </style>
