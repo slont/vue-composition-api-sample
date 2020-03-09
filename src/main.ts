d5925d2ed468bfd14e49c715e963b42053317078
slont <slont.maytry@gmail.com>
2020-03-09 14:41:59 +0900

    CompositionAPIとBuefy導入

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import './plugins/buefy'
+import './plugins/composition-api'
 
 Vue.config.productionTip = false
 
